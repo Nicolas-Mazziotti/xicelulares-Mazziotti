1ebc1eff7723b38672085f1223acecc87338e0bc
nicolas_mazziotti <nicolas.mazziotti1@gmail.com>
2022-05-15 17:07:44 -0300

    Saco un return del ItemList

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -6,14 +6,8 @@ import './ItemList.css'
 export const ItemList = ({productos}) => {
   return (
     <div className='itemContainer'>
-        {productos.map((prod) =>{
-          console.log(prod)
-            return (
-                <>
-                    <Item key={prod.id} products={prod}/>
-                </>
-            )
-        })}
+        {productos.map((prod) => <Item key={prod.id} products={prod}/>           
+        )}
     </div>
   )
 }
